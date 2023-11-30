0f6e0abd63a5f89601cfe963758572dd92178de3
Okononfua Mathew <oseghaleokononfua@gmail.com>
2023-11-30 20:33:24 +0100

    Completed!!

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import mongoose from 'mongoose';
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/user.js';
 import listingRoutes from './routes/listing.js';
+import path from 'path';
 
 dotenv.config();
 const port = process.env.PORT || 5000
@@ -25,6 +26,8 @@ const connectDb = async () => {
     }
 }
 
+const __dirname = path.resolve();
+
 // middlewares
 app.use(express.json());
 app.use(cors(corsOptions));
@@ -32,6 +35,12 @@ app.use(cookieParser());
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/listing', listingRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 app.listen(port, () => {
     connectDb();
